91f9602ec1d8120d3cd59c27e9867a82131d3173
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-03-05 16:43:46 -0300

    added State resolver

--- graphql/resolvers/states/root-query.js
+++ graphql/resolvers/states/root-query.js
@@ -1,1 +1,9 @@
-module.exports = {}
+const State = (_, { uuid }, { reply }) => {
+  const { knex } = reply
+
+  return knex('tb_states').where('uuid', uuid).first()
+}
+
+module.exports = {
+  State
+}
