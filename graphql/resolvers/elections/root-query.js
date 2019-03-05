219b1c8b4465c333a684bcd857e9ce7ad6288480
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-03-05 16:38:11 -0300

    added Election resolver

--- graphql/resolvers/elections/root-query.js
+++ graphql/resolvers/elections/root-query.js
@@ -1,5 +1,14 @@
 const Elections = (root, args, { reply }) => reply.knex('tb_elections')
 
+const Election = (_, { uuid }, { reply }) => {
+  const { knex } = reply
+
+  return knex('tb_elections')
+    .where('uuid', uuid)
+    .first()
+}
+
 module.exports = {
+  Election,
   Elections
 }
