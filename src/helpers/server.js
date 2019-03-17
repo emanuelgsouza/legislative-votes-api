34dcc15dd6d6f476cff6f334d34749e1edb1602c
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-03-17 19:50:24 -0300

    use PORT and HOST environment variables

--- src/helpers/server.js
+++ src/helpers/server.js
@@ -3,6 +3,8 @@ const databasePlugin = require('../plugins/database')
 const graphqlPlugin = require('../plugins/graphql')
 const loadRoutesPlugin = require('../plugins/routes')
 
+require('dotenv').config()
+
 const setupServer = fastify => {
   // setup config
   const server = fastify(serverConfig)
@@ -14,7 +16,7 @@ const setupServer = fastify => {
 
   // ready
   server
-    .listen(3000)
+    .listen(process.env.PORT, process.env.HOST)
     .then((address) => {
       server.log.info(`server listening on ${address}`)
     })
